9e7a3e7cc76f8c04f04b83fd7acee2272346c6a0
Sparven0 <158517054+Sparven0@users.noreply.github.com>
2024-09-19 21:27:31 +0200

    Update about.jsx

--- about.jsx
+++ about.jsx
@@ -1,1 +1,15 @@
+import React from 'react'
+import Navigation from '../Navigation'
+
+function about() {
+  return (
+    <div className='about'>
+      <h1>About page!</h1>
+      <p>Next step: figure out how to make the axios and form component work in react or write OnClick-function to hide Login on all other pages.</p>
+      
+    </div>
+  )
+}
+
+export default about
 
