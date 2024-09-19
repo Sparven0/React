568f2bc03adaddb90be13661701d5b306fb28b08
Sparven0 <158517054+Sparven0@users.noreply.github.com>
2024-09-19 21:25:05 +0200

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home'>
+      <h1>React & Axios</h1>
+      <h3>This is a test page for combining ReactJS and Axios HTTP handling, React Props, routing & more</h3>
+      <p>Feel free to log in. The dummy-email is 'eve.holt@reqres.in' and the password is 'cityslicka'</p>
+      
+    </div>
+  )
+}
+
+export default Home
